f54f211920ff57767bcd40a84256f3d6ab6efb12
Nishant Mishra <nish-nm@github.com>
2017-09-15 01:41:09 +0530

    Flashlight
    A torch themed flashlight made up of CSS,JavaScript and HTML basics.

---
+++ CSS.js
@@ -0,0 +1,1 @@
+$(function(){ $("#button").click(function(){ $(this).toggleClass("down"); $(this).toggleClass("up"); $("#light").toggleClass("on"); $("#light").toggleClass("off"); $("#front").toggleClass("lit"); $("#front").toggleClass("dark"); }); });
